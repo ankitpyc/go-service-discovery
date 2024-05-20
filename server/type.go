be12ff583b2d77020767875c97176822e8dabdd0
misanki <ankitmishra471@gmail.com>
2024-05-20 17:16:31 +0530

    added doc for server

--- server/type.go
+++ server/type.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 )
 
+// Server represents a TCP server that manages cluster details and handles connections.
 type Server struct {
 	Host           string
 	Port           string
@@ -14,11 +15,13 @@ type Server struct {
 	ClusterDetails []*clusters.ClusterConfig
 }
 
+// NewServer initializes a new Server instance with the given host and port.
+// It also initializes an empty list of cluster configurations and an RWMutex for synchronization.
 func NewServer(host string, port string) *Server {
 	return &Server{
 		Host:           host,
 		Port:           port,
-		ClusterDetails: []*clusters.ClusterConfig{},
-		SSMu:           sync.RWMutex{},
+		ClusterDetails: []*clusters.ClusterConfig{}, // Initialize an empty slice for cluster configurations
+		SSMu:           sync.RWMutex{},              // Initialize the RWMutex
 	}
 }
