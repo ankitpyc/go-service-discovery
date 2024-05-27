3591460c44ceee5b84280e5e4774ffd485fec717
misanki <ankitmishra471@gmail.com>
2024-05-27 11:11:03 +0530

    Added PING Latency

--- prober/prober.go
+++ prober/prober.go
@@ -9,12 +9,13 @@ import (
 	"go-service-discovery/cluster/events"
 	"net/http"
 	"sync/atomic"
+	"time"
 )
 
-func (prober *ProberService) MonitorForFailedChecks(ctx context.Context) {
+func (probe *ProberService) MonitorForFailedChecks(ctx context.Context) {
 	for {
 		select {
-		case member := <-prober.FailedChecks:
+		case member := <-probe.FailedChecks:
 			fmt.Printf("Node %s is unhealthy\n", member.ClusterMember.NodeAddr+":"+member.ClusterMember.NodePort)
 			if atomic.LoadInt32(&member.ClusterMember.MissedHeartbeats) >= 2 {
 				member.ClusterMember.NodeStatus = "Unreachable"
@@ -30,51 +31,53 @@ func (prober *ProberService) MonitorForFailedChecks(ctx context.Context) {
 	}
 }
 
-func (prober *ProberService) ClusterHealthCheck(ctx context.Context, config *config.ClusterDetails) {
+func (probe *ProberService) ClusterHealthCheck(ctx context.Context, config *config.ClusterDetails) {
 	config.RLock()
 	defer config.RUnlock()
 	for _, mem := range config.ClusterMemList {
-		checkCtx, cancel := context.WithTimeout(ctx, prober.TimeOut)
+		checkCtx, cancel := context.WithTimeout(ctx, probe.TimeOut)
 		defer cancel()
 
 		url := "http://" + mem.NodeAddr + ":" + mem.NodePort + "/Health"
 		req, err := http.NewRequestWithContext(checkCtx, "GET", url, nil)
 		if err != nil {
-			prober.FailedChecks <- FailedMemConfig{mem, config}
+			probe.FailedChecks <- FailedMemConfig{mem, config}
 			continue
 		}
+		reqStartTime := time.Now()
 		resp, err := http.DefaultClient.Do(req)
-		probeFailed := prober.handleProbeResponse(resp, err, mem, config)
+		reqEndTime := time.Since(reqStartTime).Milliseconds()
+		probeFailed := probe.handleProbeResponse(resp, err, mem, config)
 		if probeFailed {
 			continue
 		}
-		fmt.Printf("Node %s:%s is healthy\n", mem.NodeAddr, mem.NodePort)
+		fmt.Printf("Node %s:%s is healthy  , PING Latencty : %d MS \n ", mem.NodeAddr, mem.NodePort, reqEndTime)
 	}
 }
 
-func (prober *ProberService) handleProbeResponse(resp *http.Response, err error, mem *cluster.ClusterMember, config *config.ClusterDetails) bool {
+func (probe *ProberService) handleProbeResponse(resp *http.Response, err error, mem *cluster.ClusterMember, config *config.ClusterDetails) bool {
 	var probeFailed bool = false
 
 	if errors.Is(err, context.DeadlineExceeded) {
-		prober.handleTimeout(mem, config)
+		probe.handleTimeout(mem, config)
 		probeFailed = true
 		return probeFailed
 	}
 
 	if resp == nil || err != nil {
-		prober.FailedChecks <- FailedMemConfig{mem, config}
+		probe.FailedChecks <- FailedMemConfig{mem, config}
 		probeFailed = true
 		return probeFailed
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		prober.FailedChecks <- FailedMemConfig{mem, config}
+		probe.FailedChecks <- FailedMemConfig{mem, config}
 		probeFailed = true
 	}
 	return probeFailed
 }
 
-func (prober *ProberService) handleTimeout(mem *cluster.ClusterMember, config *config.ClusterDetails) {
+func (probe *ProberService) handleTimeout(mem *cluster.ClusterMember, config *config.ClusterDetails) {
 	fmt.Printf("Health check for node %s:%s timed out\n", mem.NodeAddr, mem.NodePort)
-	prober.FailedChecks <- FailedMemConfig{mem, config}
+	probe.FailedChecks <- FailedMemConfig{mem, config}
 }
